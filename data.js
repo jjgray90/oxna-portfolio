987e8246d9d5f670cc6a7235d3a8f3ba13d43112
Your Name <jjgray90@gmail.com>
2022-08-01 16:43:53 +0100

    updated about me & added punk API proj

--- data.js
+++ data.js
@@ -18,6 +18,15 @@ const dataArr = [
     imgAlt: "Calculator",
   },
   {
+    title: "Punk API",
+    skillTags: ["React", "API"],
+    text: "Tasked with using the Punk API to fetch beers based on user input parameters. Built with React, a good lesson in asynchronous behaviour.",
+    gitHub: "https://github.com/jjgray90/punk-api",
+    liveSite: "https://punk-api.jjgray.me/",
+    img: "assets/images/punk-api.png",
+    imgAlt: "Screenshot of punk api website",
+  },
+  {
     title: "Morse Translator",
     skillTags: ["Javascript", "TDD"],
     text: "A tech test style problem, I was tasked with adopting a TDD approach to build a morse code translator. It translates both ways.",
@@ -28,7 +37,7 @@ const dataArr = [
   },
   {
     title: "Bojo Covo",
-    skillTags: ["javascript"],
+    skillTags: ["Javascript"],
     text: "One lockdown weekend in March 2020, I decided to make a coronavirus version of space invaders. There are a couple of bugs I need to fix.",
     gitHub: "https://github.com/jjgray90/bojo-covo",
     liveSite: "https://bojo-covo.jjgray.me/",
